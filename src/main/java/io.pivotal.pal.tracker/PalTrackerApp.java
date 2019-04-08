5ed77148ef1b13b6f7cd95631e2b0a79f35d19c6
pal-1 <santhosh.kumarva@dell.com>
2019-04-08 15:16:16 +0530

    CF hello world

---
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApp.java
@@ -0,0 +1,12 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalTrackerApp {
+    public static void main(String[] ar)
+    {
+        SpringApplication.run(PalTrackerApp.class);
+    }
+}
